1050dfe26188487bccab41ba54d6e67ce4e6d57c
Emmanuelamr115 <Emmanuelamr2004@outlook.com>
2024-11-05 07:51:47 -0600

    Add files via upload

---
+++ MyJFrame.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myjframe;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Emmanuel
+ */
+
+public class MyJFrame extends JFrame {
+    private final JLabel myLabel;
+
+    public MyJFrame() {
+
+        myLabel = new JLabel("Hola Mundo");
+        add(myLabel);
+
+        setTitle("Mi JFrame");
+        setSize(170, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null); 
+    }
+
+    public static void main(String[] args) {
+        MyJFrame frame = new MyJFrame();
+        frame.setVisible(true);
+    }
+}
